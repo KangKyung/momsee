8d1a13c03bef33eb07b16e3580de184c618e9cdd
gjgustjd <44759382+gjgustjd@users.noreply.github.com>
2018-12-28 20:46:15 +0900

    사인업 코드
    ㅇㅇ

--- momsee2/app/src/main/java/com/example/jiinheo/momsee/SignupActivity.java
+++ momsee2/app/src/main/java/com/example/jiinheo/momsee/SignupActivity.java
@@ -2,12 +2,28 @@ package com.example.jiinheo.momsee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SignupActivity extends AppCompatActivity {
-
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
+    Button button;
+    EditText Name,password,passwordCheck,email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        button = (Button)findViewById(R.id.button_OK);
+        button.setOnClickListener(this);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        passwordCheck = (EditText)findViewById(R.id.passwordcheck);
+        Name = (EditText)findViewById(R.id.UserName);
     }
+    public void onClick(View v){
+        if(v.getId() == R.id.button){
+            /*데이터 DB로 넘길 부분*/
+        }
+    }
+
 }
