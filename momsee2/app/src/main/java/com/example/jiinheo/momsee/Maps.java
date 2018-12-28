943751fcdee98302443ae13f8cc4d8ea57ebd9dd
gjgustjd <44759382+gjgustjd@users.noreply.github.com>
2018-12-28 22:15:00 +0900

    지도 현위치 확인 완성
    ㅇㅇ

--- momsee2/app/src/main/java/com/example/jiinheo/momsee/Maps.java
+++ momsee2/app/src/main/java/com/example/jiinheo/momsee/Maps.java
@@ -8,39 +8,45 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Maps extends FragmentActivity implements OnMapReadyCallback {
-
-    private static final String TAG = "MainActivity";
-
+    GoogleMap googleMap;
+    MapFragment mapFragment;
     LocationManager locationManager;
-    MapView mapView;
-    SupportMapFragment mapFragment;
-    GoogleMap map;
+    RelativeLayout boxMap;
+    //나의 위도 경도 고도
     double mLatitude;  //위도
     double mLongitude; //경도
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        boxMap = (RelativeLayout)findViewById(R.id.boxMap);
+
+        //LocationManager
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
 
         //GPS가 켜져있는지 체크
         if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
@@ -68,6 +74,25 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
             requestMyLocation();
         }
     }
+
+    //권한 요청후 응답 콜백
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //ACCESS_COARSE_LOCATION 권한
+        if(requestCode==1){
+            //권한받음
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                requestMyLocation();
+            }
+            //권한못받음
+            else{
+                Toast.makeText(this, "권한없음", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    //나의 위치 요청
     public void requestMyLocation(){
         if(ContextCompat.checkSelfPermission(Maps.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(Maps.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
@@ -76,11 +101,13 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         //요청
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 10, locationListener);
     }
+
+    //위치정보 구하기 리스너
     LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-            if (ContextCompat.checkSelfPermission(Maps.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
-                    ContextCompat.checkSelfPermission(Maps.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if(ContextCompat.checkSelfPermission(Maps.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(Maps.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
                 return;
             }
             //나의 위치를 한번만 가져오기 위해
@@ -91,34 +118,43 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
             mLongitude = location.getLongitude(); //경도
 
             //맵생성
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
             //콜백클래스 설정
             mapFragment.getMapAsync(Maps.this);
-
         }
 
         @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            Log.d("gps", "onStatusChanged");
-        }
+        public void onStatusChanged(String provider, int status, Bundle extras) { Log.d("gps", "onStatusChanged"); }
 
         @Override
-        public void onProviderEnabled(String provider) {
-        }
+        public void onProviderEnabled(String provider) { }
 
         @Override
-        public void onProviderDisabled(String provider) {
-        }
+        public void onProviderDisabled(String provider) { }
     };
 
+    //구글맵 생성 콜백
     @Override
-    public void onMapReady(GoogleMap googleMap){
-        map = googleMap;
-        LatLng seoul = new LatLng(37.52487,126.92723);
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(seoul).title("원하는 위치에 마커를 표시했습니다.");
-        map.addMarker(markerOptions);
-        map.moveCamera(CameraUpdateFactory.newLatLng(seoul));
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
 
+        //지도타입 - 일반
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //나의 위치 설정
+        LatLng position = new LatLng(mLatitude , mLongitude);
+
+        //화면중앙의 위치와 카메라 줌비율
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15));
+        //지도 보여주기
+        boxMap.setVisibility(View.VISIBLE);
+        MarkerOptions makerOptions = new MarkerOptions();
+        makerOptions // LatLng에 대한 어레이를 만들어서 이용할 수도 있다.
+                .position(position)
+                .title("현위치"); // 타이틀.
+
+        // 2. 마커 생성 (마커를 나타냄)
+        this.googleMap.addMarker(makerOptions);
     }
+
 }
