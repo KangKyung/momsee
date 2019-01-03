a4e0b926de85781f754538644fdaf2a509d4eb1a
heojiin <39071796+heojiin@users.noreply.github.com>
2019-01-03 16:09:25 +0900

    +자식네임,자식메인
    자식네임 ui, 자식메인ui 추가
    
    child_info_item을 activity_childinfo_itme으로
    children_infto를 activity_children_info로
    레이아웃 이름 통일

--- momsee2/app/src/main/java/com/example/jiinheo/momsee/child_chatting.java
+++ momsee2/app/src/main/java/com/example/jiinheo/momsee/child_chatting.java
@@ -1,6 +1,5 @@
 package com.example.jiinheo.momsee;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +16,7 @@ public class child_chatting extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.children_info);
+        setContentView(R.layout.activity_children_info);
 
         ArrayList<String> MyListView = new ArrayList<String>();
         MyListView.add("강경훈과의 채팅");
