6c07c26036db7cf423944fec97053085da56f0ce
KangKyung <38858863+KangKyung@users.noreply.github.com>
2019-01-23 15:41:25 +0900

    kiost

---
+++ KioskProject/app/src/main/java/com/example/rudgn/kioskproject/MyAdmin.java
@@ -0,0 +1,11 @@
+package com.example.rudgn.kioskproject;
+
+import android.app.admin.DeviceAdminReceiver;
+
+/**
+ * Created by rudgn on 2019-01-23.
+ */
+
+public class MyAdmin extends DeviceAdminReceiver {
+
+}
